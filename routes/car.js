eb10ddbfd65fbdd7d7cbef8131ccbd97b75935c0
praneethkumar695 <praneethkumar695@gmail.com>
2021-11-03 22:38:02 -0500

    Updated car.js

--- routes/car.js
+++ routes/car.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('car', { title: 'Search Results' });
+  res.render('car', { title: 'Search Results Car' });
 });
 
 module.exports = router;
